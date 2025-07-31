e6cd459d2bb9dd396ade73d9e8a7351af5e9d975
Hannes Domani <ssbssa@yahoo.de>
2025-07-31 19:52:24 +0200

    update copyright years

--- mgwhelp/dwarf_pe.h
+++ mgwhelp/dwarf_pe.h
@@ -1,6 +1,6 @@
 /*
  * Copyright 2012 Jose Fonseca
- * Copyright (C) 2013-2019 Hannes Domani
+ * Copyright (C) 2013-2025 Hannes Domani
  *
  * This library is free software; you can redistribute it and/or
  * modify it under the terms of the GNU Lesser General Public
